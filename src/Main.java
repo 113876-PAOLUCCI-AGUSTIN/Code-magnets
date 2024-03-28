1f68daba6dcb42d774e50d14db13ac6a8dacca18
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-28 17:13:43 -0300

    Code-magnets. DONE.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int x = 3; //1
+
+        while(x>0) {  //2
+            if (x > 2) { //3
+                System.out.print("a");
+            }
+
+            x = x-1;   // 4
+            System.out.print("-");
+
+            if (x == 2){  // 5
+                System.out.print("b c");
+            }
+            if (x == 1){ // 6
+                System.out.print("d");
+                x = x-1;
+           }
+        }
     }
 }
